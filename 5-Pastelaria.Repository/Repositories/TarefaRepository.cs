11fe1dc0fc5c335908a3fbe2565f701787297c14
Wesley Silveira de Paula <wesleysdepaula@outlook.com>
2021-07-08 16:14:02 -0300

    Começando a implementação das Tarefas

---
+++ 5-Pastelaria.Repository/Repositories/TarefaRepository.cs
@@ -0,0 +1,55 @@
+﻿using Pastelaria.Domain.Tarefa.Dto;
+using ProjetoBanco.Infra.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Pastelaria.Repository.Repositories
+{
+    public class TarefaRepository
+    {
+        private readonly Conexao conexao;
+
+        public TarefaRepository()
+        {
+            conexao = new Conexao();
+        }
+
+        private enum Procedures
+        {
+            PSP_InsTarefa,
+            PSP_SelTarefaPorUsuario
+        }
+
+        public void Post(TarefaDto tarefa)
+        {
+            conexao.ExecuteProcedure(Procedures.PSP_InsTarefa);
+            conexao.AddParameter("@IdGestor", tarefa.IdGestor);
+            conexao.AddParameter("@IdUsuario", tarefa.IdUsuario);
+            conexao.AddParameter("@TarefaDescricao", tarefa.TarefaDescricao);
+            conexao.AddParameter("@DataAgendamento", tarefa.DataAgendamento);
+            conexao.AddParameter("@DataLimiteExecucao", tarefa.DataLimiteExecucao);
+            conexao.AddParameter("@DataExecucao", tarefa.DataExecucao);
+            conexao.ExecuteNonQuery();
+        }
+
+        public TarefaDto GetTarefaPorIdUsuario(int idUsuario)
+        {
+            conexao.ExecuteProcedure(Procedures.PSP_SelTarefaPorUsuario);
+            conexao.AddParameter("@IdUsuario", idUsuario);
+            using (var r = conexao.ExecuteReader())
+            {
+                if(r.Read())
+                {
+                    return new TarefaDto
+                    {
+
+                    };
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Scripts/Tarefa.sql
@@ -0,0 +1,111 @@
+
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[PSP_SelUsuarioPorEmaileSenha]') AND objectproperty(id, N'IsPROCEDURE')=1)
+	DROP PROCEDURE [dbo].[PSP_SelUsuarioPorEmaileSenha]
+GO
+
+CREATE PROCEDURE [dbo].[PSP_SelUsuarioPorEmaileSenha]
+	@email varchar(70),
+	@senha varchar(30)
+	AS
+
+	/*
+	Documentacao
+	Arquivo Fonte.....: Login.sql
+	Objetivo..........: Selecionar Usuário por email e senha
+	Autor.............: SMN - Wesley Silveira
+ 	Data..............: 08/07/2021
+	Ex................: EXEC [dbo].[PSP_SelUsuarioPorEmaileSenha]'oportunidade@smn.com.br','teste123'
+
+	*/
+
+	BEGIN;
+
+		-- nolock em todas tabelas da proc
+		SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+		SELECT	Id,
+				Nome,
+				Email,
+				Senha
+			FROM [dbo].[Usuario]
+			WHERE Email = @email and
+				  Senha = @senha
+
+	END;
+GO
+
+
+
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[PSP_InsUsuario]') AND objectproperty(id, N'IsPROCEDURE')=1)
+	DROP PROCEDURE [dbo].[PSP_InsUsuario]
+GO
+
+CREATE PROCEDURE [dbo].[PSP_InsUsuario]
+	@CodigoTipoUsuario tinyint,
+	@Email varchar(70),
+	@Senha varchar(30),
+	@Nome varchar(80),
+	@DataNascimento date = null,
+	@TelefoneFixo varchar(10) = null,
+	@TelefoneCelular varchar(11) = null,
+	@Logradouro varchar(80) = null,
+	@Bairro varchar(50) = null,
+	@Cidade varchar(50) = null,
+	@Uf varchar(2) = null,
+	@Cep varchar(8) = null,
+	@Foto varchar(30) = null
+	AS
+
+	/*
+	Documentacao
+	Arquivo Fonte.....: Usuario.sql
+	Objetivo..........: Inserir um novo usuário no Cadastro
+	Autor.............: SMN - Wesley Silveira
+ 	Data..............: 08/07/2021
+	Ex................: EXEC [dbo].[PSP_InsUsuario]
+
+	*/
+
+	BEGIN;
+
+		INSERT INTO Usuario (CodigoTipoUsuario, Email, Senha, Nome, DataNascimento, 
+							TelefoneFixo, TelefoneCelular, Logradouro, Bairro, Cidade, Uf, Cep, Foto)
+			VALUES (@CodigoTipoUsuario,@Email,@Senha,@Nome,@DataNascimento,
+					@TelefoneFixo,@TelefoneCelular,@Logradouro,@Bairro,@Cidade,@Uf,@Cep,@Foto)
+
+	END;
+GO
+
+
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[PSP_SelUsuarioPorEmail]') AND objectproperty(id, N'IsPROCEDURE')=1)
+	DROP PROCEDURE [dbo].[PSP_SelUsuarioPorEmail]
+GO
+
+CREATE PROCEDURE [dbo].[PSP_SelUsuarioPorEmail]
+	@Email varchar(70)
+	AS
+
+	/*
+	Documentacao
+	Arquivo Fonte.....: Usuario.sql
+	Objetivo..........: Buscar um usuário na tabela pelo código informado
+	Autor.............: SMN - Wesley Silveira
+ 	Data..............: 08/07/2021
+	Ex................: EXEC [dbo].[PSP_SelUsuarioPorEmail]
+
+	*/
+
+	BEGIN;
+
+		-- nolock em todas tabelas da proc
+		SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+		SELECT  Id, CodigoTipoUsuario, Email, Senha, Nome, DataNascimento, 
+				TelefoneFixo, TelefoneCelular, Logradouro, Bairro, Cidade, Uf, Cep, Foto
+			FROM [dbo].[Usuario]
+			WHERE Email = @Email
+
+	END;
+GO
+				
+								
